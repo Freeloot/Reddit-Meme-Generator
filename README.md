cf29397665d34bcd0bbe0493796eab9a2f50e425
Infamy <65705916+Freeloot@users.noreply.github.com>
2022-06-01 16:03:06 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Reddit-Meme-Generator
-Download memes from reddit!
+# Reddit Meme Generator
+Reddit Meme Generator is a CLI based script that gets the hottest r/dankmemes post and downloads it.
+
+## How does it work?
+
+- Reddit Meme Generator uses Python's native requests library and aiohttp to scrape and download reddit posts.
+
+1. **DOWNLOAD**
+- Clone the repository.
+```
+$ git clone https://github.com/Freeloot/Reddit-Meme-Generator
+$ cd Reddit-Meme-Generator/
+```
+
+2. **EXECUTE**
+
+- Run from source
+```
+$ cd src
+$ python3 meme.py
+```
---
+++ src/meme.py
@@ -0,0 +1,39 @@
+import aiohttp, asyncio, random, requests, socket
+
+def is_connected(hostname):
+  try:
+    host = socket.gethostbyname(hostname)
+    s = socket.create_connection((host, 80), 2)
+    s.close()
+    return True
+  except:
+     pass
+  return False
+
+async def meme():
+    global res
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+            res =  await r.json()
+            return res['data']['children'] [random.randint(0, 25)]['data']['url']
+
+def main():
+    global image
+    if is_connected("1.1.1.1") == False:
+        print('! Internet Connection is not present.')
+        exit()
+    elif is_connected("1.1.1.1") == True:
+        print('> Internet Connection is present, proceeding with program execution.\n')
+        pass
+
+    img_url = asyncio.get_event_loop().run_until_complete(meme()) 
+    img_data = requests.get(img_url)
+    print('> ' + img_url)
+
+    try:
+        with open(f'reddit_meme.{img_url[-3:]}', 'wb') as handler: handler.write(img_data.content)
+        print(f'> Meme successfully saved to reddit_meme.{img_url[-3:]}')
+    except:
+        pass
+
+if __name__ == '__main__': main()
